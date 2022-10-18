cb3291c2c66e394df031cee05577646b0439c3ea
hokita <hideee.0202@gmail.com>
2022-10-18 21:23:08 +0900

    First Commit.

---
+++ src/main/kotlin/dev/hokita/mini/controller/HomeController.kt
@@ -0,0 +1,24 @@
+package dev.hokita.mini.controller
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+    @Value("\${my.name}")
+    val name: String = "Karashi"
+
+    @Value("\${my.age}")
+    val age: Int = 4
+
+    data class User(
+        val name: String,
+        val age: Int
+    )
+
+    @GetMapping("/")
+    suspend fun homeGet(): User {
+        return User(name, age)
+    }
+}
---
+++ src/test/kotlin/dev/hokita/mini/MiniApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hokita.mini
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
